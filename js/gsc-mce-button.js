0ef78b02b0573af37f36a83cb28fa186b9ef3863
spwebguy <spwebguy@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-11 04:24:43 +0000

    fixed shortcodes.
    git-svn-id: https://plugins.svn.wordpress.org/grid-shortcodes/trunk@1178717 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/gsc-mce-button.js
+++ js/gsc-mce-button.js
@@ -32,31 +32,31 @@
                 {
 				    text: 'Four quarters',
 				    onclick: function() {
-				    	editor.insertContent('[GDC_row]<br/>[GDC_column size="two-thirds"]<br/>Your content here<br/>[/GDC_column]<br/>[GDC_column size="third"]<br/>Your content here<br/>[/GDC_column]<br/>[/GDC_row]');
+				    	editor.insertContent('[GDC_row]<br/>[GDC_column size="quarter"]<br/>Your content here<br/>[/GDC_column]<br/>[GDC_column size="quarter"]<br/>Your content here<br/>[/GDC_column]<br/>[GDC_column size="quarter"]<br/>Your content here<br/>[/GDC_column]<br/>[GDC_column size="quarter"]<br/>Your content here<br/>[/GDC_column]<br/>[/GDC_row]');
 				    },
                 },
                 {
 				    text: 'A half & two quarters',
 				    onclick: function() {
-				    	editor.insertContent('[GDC_row]<br/>[GDC_column size="two-thirds"]<br/>Your content here<br/>[/GDC_column]<br/>[GDC_column size="third"]<br/>Your content here<br/>[/GDC_column]<br/>[/GDC_row]');
+				    	editor.insertContent('[GDC_row]<br/>[GDC_column size="half"]<br/>Your content here<br/>[/GDC_column]<br/>[GDC_column size="quarter"]<br/>Your content here<br/>[/GDC_column]<br/>[GDC_column size="quarter"]<br/>Your content here<br/>[/GDC_column]<br/>[/GDC_row]');
 				    },
                 },
                 {
 				    text: 'Two quarters & a half',
 				    onclick: function() {
-				    	editor.insertContent('[GDC_row]<br/>[GDC_column size="two-thirds"]<br/>Your content here<br/>[/GDC_column]<br/>[GDC_column size="third"]<br/>Your content here<br/>[/GDC_column]<br/>[/GDC_row]');
+				    	editor.insertContent('[GDC_row]<br/>[GDC_column size="quarter"]<br/>Your content here<br/>[/GDC_column]<br/>[GDC_column size="quarter"]<br/>Your content here<br/>[/GDC_column]<br/>[GDC_column size="half"]<br/>Your content here<br/>[/GDC_column]<br/>[/GDC_row]');
 				    },
                 },
                 {
 				    text: 'Three quarters & a quarter',
 				    onclick: function() {
-				    	editor.insertContent('[GDC_row]<br/>[GDC_column size="two-thirds"]<br/>Your content here<br/>[/GDC_column]<br/>[GDC_column size="third"]<br/>Your content here<br/>[/GDC_column]<br/>[/GDC_row]');
+				    	editor.insertContent('[GDC_row]<br/>[GDC_column size="three-quarters"]<br/>Your content here<br/>[/GDC_column]<br/>[GDC_column size="quarter"]<br/>Your content here<br/>[/GDC_column]<br/>[/GDC_row]');
 				    },
                 },
                 {
 				    text: 'A quarter & three quarters',
 				    onclick: function() {
-				    	editor.insertContent('[GDC_row]<br/>[GDC_column size="two-thirds"]<br/>Your content here<br/>[/GDC_column]<br/>[GDC_column size="third"]<br/>Your content here<br/>[/GDC_column]<br/>[/GDC_row]');
+				    	editor.insertContent('[GDC_row]<br/>[GDC_column size="quarter"]<br/>Your content here<br/>[/GDC_column]<br/>[GDC_column size="three-quarters"]<br/>Your content here<br/>[/GDC_column]<br/>[/GDC_row]');
 				    },
                 },
 			]
